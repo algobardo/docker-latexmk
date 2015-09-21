28707a259fe4abafbdb92f24f2da934e47d5fbe6
Hannes Eichblatt <mail@hanneseichblatt.de>
2015-09-21 10:19:35 +0200

    Fix: sometimes LatexMK wouldn't run with -c

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 # Run latexmk on given files,
 # then chmod the results to the source's owner.
 
-/usr/bin/latexmk -pdf -c "$1"
+/usr/bin/latexmk -pdf "$1"
 chown -v $(stat --format=%u "$1"):$(stat --format=%g "$1") $(basename "$1" .tex).*
