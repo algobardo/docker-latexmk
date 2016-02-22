6f4e6b7e14ef948924c72019ff795298979e2f23
algobardo <algobardo@users.noreply.github.com>
2016-02-22 17:02:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,1 @@
 #!/usr/bin/bash -x
-
-# Run latexmk on given files,
-# then chmod the results to the source's owner.
-
-/usr/bin/latexmk -pdf "$1"
-chown -v $(stat --format=%u "$1"):$(stat --format=%g "$1") $(basename "$1" .tex).*
